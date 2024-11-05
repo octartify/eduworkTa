7da82298ffa972709f9d3edc15cec5374b2485e0
octartify <108569572+octartify@users.noreply.github.com>
2024-11-05 16:12:51 +0700

    Add files via upload

---
+++ Katalon/Scripts/HDS app/AddProduct/Script1730781481739.groovy
@@ -0,0 +1,31 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+Mobile.startApplication('D:\\My Data\\Downloads\\Happy Day Shopping1.7.apk', true)
+
+Mobile.tap(findTestObject('Object Repository/android.widget.TextView - NEW PRODUCTS'), 0)
+
+Mobile.tap(findTestObject('Object Repository/Product-ajinomoto'), 0)
+
+Mobile.tap(findTestObject('Object Repository/AddtoBag-button'), 0)
+
+Mobile.tap(findTestObject('Object Repository/cartBag-button'), 0)
+
+Mobile.closeApplication()
+
